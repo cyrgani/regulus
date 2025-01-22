607119b606101425a9c14e8cbbe30f4d1c8c6608
cyrgani <ansgar.w.zielke@gmail.com>
2025-01-22 22:34:20 +0100

    simplify `std_modules!`

--- src/stdlib/mod.rs
+++ src/stdlib/mod.rs
@@ -1,27 +1,15 @@
-macro_rules! collect_builtins {
-    ($($name: ident),*) => {
-        pub fn all_functions() -> std::collections::HashMap<String, $crate::Atom> {
-            let mut functions = std::collections::HashMap::new();
-
-            for module in [
-                $($name::functions(),)*
-            ] {
-                for (name, function) in module {
-                    functions.insert(name.to_string(), $crate::Atom::Function(function));
-                }
-            }
-
-            functions
-        }
-
-    };
-}
-
 macro_rules! stl_modules {
     ($($name: ident),*) => {
         $(pub mod $name;)*
-        collect_builtins! {
-            $($name),*
+
+        pub fn all_functions() -> std::collections::HashMap<String, $crate::Atom> {
+            let mut functions = std::collections::HashMap::new();
+
+            $(for (name, function) in $name::functions() {
+                functions.insert(name.to_string(), $crate::Atom::Function(function));
+            })*
+
+            functions
         }
     };
 }
