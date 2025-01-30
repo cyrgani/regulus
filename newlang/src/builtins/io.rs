240894a7bd3ef0d2d0a5638ee208424d436f2f37
cyrgani <ansgar.w.zielke@gmail.com>
2025-01-30 11:29:31 +0100

    document `io` module

--- newlang/src/builtins/io.rs
+++ newlang/src/builtins/io.rs
@@ -5,6 +5,10 @@ fn write_to_stdout(state: &mut State, msg: &str) {
 }
 
 functions! {
+    /// Evaluates all given arguments and prints them to stdout.
+    /// All arguments are separated with a single space.
+    /// After all arguments have been printed, a newline is also printed.
+    /// Returns `null`.
     "print"(_) => |state, args| {
         for arg in args {
             let arg_val = arg.eval(state)?;
@@ -13,6 +17,8 @@ functions! {
         write_to_stdout(state, "\n");
         Ok(Atom::Null)
     }
+    /// Takes no arguments and reads from stdin until a newline is entered.
+    /// Returns the read input, including the newline, as a string.
     "input"(0) => |state, _| {
         let mut input = String::new();
         match state.stdin.read_line(&mut input) {
@@ -22,9 +28,13 @@ functions! {
             }
         }
     }
+    /// Prints the debug representation of the given argument to stdout, followed by a newline.
+    /// 
+    /// NOTE: the output format of this method is unstable.
+    /// NOTE: this method may be removed in the future.
     "debug"(1) => |state, args| {
         let arg_val = args[0].eval(state)?;
-        write_to_stdout(state, &format!("Debug: {arg_val:?}\n"));
+        write_to_stdout(state, &format!("{arg_val:?}\n"));
         Ok(Atom::Null)
     }
 }
