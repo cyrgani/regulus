31db92064878182992f840e3c46058d6d5a81d73
cyrgani <ansgar.w.zielke@gmail.com>
2025-09-02 14:17:35 +0200

    move `string` into the STL

--- regulus/src/builtins/cast.rs
+++ regulus/src/builtins/cast.rs
@@ -1,10 +1,6 @@
 use crate::exception::TypeError;
 use crate::prelude::*;
 
-fn cast_error_builder(atom: &Atom, new_type: &str, state: &State) -> Exception {
-    state.raise(TypeError, format!("Unable to cast {atom} to {new_type}"))
-}
-
 // TODO: try making as many of these as possible stl functions
 functions! {
     // TODO: implement this directly in the STL
@@ -12,21 +8,12 @@ functions! {
     "__builtin_str_to_int"(1) => |state, args| {
         args[0].eval_string(state)?.parse().map(Atom::Int).map_err(|e| state.raise(TypeError, format!("cannot convert string to int: {e}")))
     }
-    /// Converts the given value into a string, raising an exception if it is not possible to cast.
-    ///
-    /// This method is fallible and is currently only able to cast ints, bools, strings and nulls (to "null").
-    /// If you want to display an arbitrary atom (such as for error messages), use `printable(1)`
-    /// instead, which is infallible.
-    "string"(1) => |state, args| {
-        let atom = args[0].eval(state)?.into_owned();
-        Ok(match atom {
-            Atom::Int(val) => Atom::String(val.to_string()),
-            Atom::Bool(val) => Atom::String(val.to_string()),
-            Atom::String(_) => atom,
-            Atom::Null => Atom::String("null".to_string()),
-            _ => return Err(cast_error_builder(&atom, "string", state)),
-        })
+    // TODO: implement this directly in the STL
+    /// Converts the given integer into a string.
+    "__builtin_int_to_str"(1) => |state, args| {
+        Ok(Atom::String(args[0].eval_int(state)?.to_string()))
     }
+    // TODO: invent some way for objects to define how they want to be printed.
     /// Evaluates the given arg and returns a string representation of it.
     /// See the documentation of `string(1)` for a comparison of these two methods.
     /// Note that the exact output format is not yet stable and may change, especially regarding
--- regulus/stdlib/casting.re
+++ regulus/stdlib/casting.re
@@ -19,4 +19,17 @@ def(int, val, switch(type_id(val),
     BOOL_TY_ID, ifelse(val, 1, 0),
     STRING_TY_ID, __builtin_str_to_int(val),
     error("Type", strconcat("cannot cast ", printable(val), " to int"))
-))
+)),
+
+# Converts the given value into a string, raising an exception if it is not possible to cast.
+#
+# This method is fallible and is currently only able to cast ints, bools, strings and nulls (to "null").
+# If you want to display an arbitrary atom (such as for error messages), use `printable(1)`
+# instead, which is infallible.
+def(string, val, switch(type_id(val),
+    INT_TY_ID, printable(val),
+    BOOL_TY_ID, ifelse(val, "true", "false"),
+    STRING_TY_ID, val,
+    NULL_TY_ID, "null",
+    error("Type", strconcat("cannot cast ", printable(val), " to string"))
+)),
